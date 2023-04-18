2035a9fd9b9865c462322fe69a27b9e3a7955d1e
eejeong <wdwaaa7325@gmail.com>
2023-04-18 15:07:12 +0900

    0418 IoC

---
+++ day01/src/main/java/com/kbstar/app/App.java
@@ -0,0 +1,9 @@
+package kbstar.app;
+
+public class App {
+    public static void main(String args[]){
+        TV tv = new LTV();
+        tv.turnOn();
+
+    }
+}
---
+++ day01/src/main/java/com/kbstar/frame/TV.java
@@ -0,0 +1,8 @@
+package kbstar.frame;
+
+//interface : 개발 표준 설계
+
+public interface TV {
+    public void turnOn();
+    public void turnOff();
+}
