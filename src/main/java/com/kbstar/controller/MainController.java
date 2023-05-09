42f7a6048002f424c2cf0a454c8f604a84585434
eejeong <wdwaaa7325@gmail.com>
2023-05-09 15:56:50 +0900

    web14

--- src/main/java/com/kbstar/controller/MainController.java
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,10 @@ import javax.servlet.http.HttpSession;
 @Slf4j
 @Controller
 public class MainController {
+
+    @Value("${adminserver}")
+    String adminserver;
+
     @Autowired
     CustService custService;
 
@@ -133,6 +138,7 @@ public class MainController {
 
     @RequestMapping("/websocket")
     public String websocket(Model model){
+        model.addAttribute("adminserver", adminserver);
         model.addAttribute("center", "websocket");
         return "index";
     }
