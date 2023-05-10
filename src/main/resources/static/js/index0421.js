84d03a6e0742a4343a09aa935b0ea53c8a4299ca
eejeong <wdwaaa7325@gmail.com>
2023-05-10 09:11:30 +0900

    web15

---
+++ src/main/resources/static/js/index0421.js
@@ -0,0 +1,27 @@
+let login_form = {
+    init:function (){
+        $('#login_btn').click(function (){
+            login_form.send();
+        });
+    },
+    send:function (){
+        $('#login_form').attr({
+            'action':'/loginimpl',
+            'method':'post'
+        });
+        $('#login_form').submit();
+    }
+};
+
+
+let jsp01 = {
+    data:0,
+    init:function (num){
+        this.data=num;
+        // this.data='<c:out value="${num}"/>';
+        this.display();
+    },
+    display:function (){
+        $('#jsp01').text(this.data+'');
+    }
+};
