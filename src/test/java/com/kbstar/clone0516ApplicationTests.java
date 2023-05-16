9b78ad87de26651e79b1760225548e9ae4103e99
eejeong <wdwaaa7325@gmail.com>
2023-05-16 19:11:01 +0900

    clone0516

---
+++ src/test/java/com/kbstar/clone0516ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class clone0516ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
